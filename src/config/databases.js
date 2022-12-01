ebfbeeb58245548d54d4d3edaf337f40310c92e6
ThurLeandro <arturbc109@gmail.com>
2022-12-01 15:30:15 -0300

    first commit

---
+++ src/config/databases.js
@@ -0,0 +1,12 @@
+module.exports = {
+	dialect: "mysql",
+	host: "localhost",
+	port: "3306",
+	username: "root",
+	password: "",
+	database: "Bd_trabalho",
+	define: {
+		timestamps: false,
+		underscored: true,
+	},
+};
